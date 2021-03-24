07979dc62fb79aae53180f3500c7da2263ea446c
Nathan Blaz <nathanblaz@gmail.com>
2021-03-24 15:43:57 -0700

    Working on anagrams

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,5 +1,20 @@
 function firstAnagram(str1, str2) {
-  // Code goes here ....
+  // for loop
+  for (let i = 0; i < str1.length; i++) {
+    // index of on second string inputting i
+    let currentIndex = str1[i];
+    let matchingIndex = str2.indexOf(currentIndex);
+    console.log(matchingIndex);
+    if (matchingIndex) {
+      // delete at that index 
+      str2.replace(`${str2[matchingIndex]}`, '');
+    }
+  }
+  if (str2.length === 0) {
+    return true;
+  }
+  else return false;
+  
 }
 
 
@@ -17,3 +32,7 @@ function thirdAnagram(str1, str2) {
 function fourthAnagram(str1, str2) {
   // Code goes here ....
 }
+
+
+// isAnagram("gizmo", "sally");    // => false
+console.log(firstAnagram("elvis", "lives"));    // => true
